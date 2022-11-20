9b5e357508cea4ef9ceb586f87cb262217a1c8d6
VictorReisDevIvip <victorreisdev@ivipcoin.com>
2022-11-20 13:22:23 -0300

    implementação de tratamento de erros - login page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuController, NavController } from '@ionic/angular';
-import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth'
+import { MenuController, NavController, ToastController, AlertController } from '@ionic/angular';
+import { getAuth, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth'
 
 
 @Component({
@@ -10,18 +10,21 @@ import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebas
 })
 export class LoginPage implements OnInit {
 
-  constructor( public menuCtrl : MenuController, public navCtrl : NavController) { }
+  constructor( public menuCtrl : MenuController, 
+    public navCtrl : NavController, 
+    public toastCtrl : ToastController,
+    public alertCtrl : AlertController) { }
 
   auth = getAuth()
   inpEmailLogin : string = ""
   inpSenhaLogin : string = ""
+  inpRecSenha   : string = ""
 
   
   ngOnInit() {
     this.menuCtrl.enable(false)
     onAuthStateChanged(this.auth, (user) => {
       if(user){
-        console.log(`usuario ${user.email} logado`)
         this.navCtrl.navigateForward("home")
       }
     })
@@ -30,16 +33,71 @@ export class LoginPage implements OnInit {
   toCadastro(){
     this.navCtrl.navigateForward("cadastro-usuario")
   }
-  //função para efetuar login
+  //efetuar login
   logar(){
-    signInWithEmailAndPassword(this.auth, this.inpEmailLogin, this.inpSenhaLogin)
+    if(this.inpEmailLogin == "" || this.inpSenhaLogin == "" ){
+      this.alertCamposVazios()
+    }else{
+      signInWithEmailAndPassword(this.auth, this.inpEmailLogin, this.inpSenhaLogin)
       .then((usuario) => {
-        console.log("logado")
         this.toHome();
       })
       .catch((erro) => {
-        console.log("Erro")
+        console.log(erro)
+        this.alertUsuarioOuSenhaIncorretos()
       })
+    }
+  }
+
+  async alertRecuperarSenha(){
+    const alert = await this.alertCtrl.create({
+      header: 'Recuperação de senha',
+      subHeader: 'Insira um email para enviar o link para recuperar sua senha',
+      inputs: [{
+        placeholder: 'Email: ',
+        type: 'email',
+        name: 'inpRecSenha'
+      }],
+      buttons: [{
+        text: 'Enviar',
+        handler: (alertData)=>{
+          sendPasswordResetEmail(this.auth, alertData.inpRecSenha)
+          .then(sucesso => {
+            this.toastRecSenhaOk()
+          }).catch(erro => {
+            
+          })
+        }
+      }]
+    })
+    await alert.present()
+  }
+
+  async alertCamposVazios(){
+    const alert = await this.alertCtrl.create({
+      header: 'Ops...',
+      subHeader: 'Favor, preencha todos os campos antes de continuar.',
+      buttons:['Ok']
+    })
+    await alert.present()
+  }
+
+  async alertUsuarioOuSenhaIncorretos(){
+    const alert = await this.alertCtrl.create({
+      header: 'Ops...',
+      subHeader: 'Verifique se o usuário e senha informados são válidos ou se ja foram previamente cadastrados no aplicativo',
+      buttons: ['Ok']
+    })
+    await alert.present()
+  }
+
+  async toastRecSenhaOk(){
+    const toast = await this.toastCtrl.create({
+      message: 'Email de recuperação enviado!',
+      duration: 1500,
+      icon: 'checkmark-circle-outline',
+    })
+    await toast.present()
   }
 
   toHome(){
